81803db2d18ee6fa5cfd64e6bc37c1d060448fa4
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-01-20 13:37:06 -0500

    Add Controller and Thymeleaf template views

---
+++ src/main/java/com/dushdesh/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package com.dushdesh.spring5webapp.controller;
+
+import com.dushdesh.spring5webapp.model.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
