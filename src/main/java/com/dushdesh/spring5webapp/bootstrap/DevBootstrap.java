52817d7614fd30f13ba7abd3430aa1c7462388c7
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-01-20 11:30:54 -0500

    Init Data on Context Refreshed event from ApplicationListner

---
+++ src/main/java/com/dushdesh/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,44 @@
+package com.dushdesh.spring5webapp.bootstrap;
+
+import com.dushdesh.spring5webapp.model.Author;
+import com.dushdesh.spring5webapp.model.Book;
+import com.dushdesh.spring5webapp.model.repository.AuthorRepository;
+import com.dushdesh.spring5webapp.model.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    @Autowired
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Drive Design", "123", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE without EJB", "345", "Worx");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
